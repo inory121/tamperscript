624ee19c410be780b6202cdff66ff8d3dd5df840
inory121 <2353539668@qq.com>
2024-02-11 12:27:42 +0800

    修改@match为所有页面，修复部分代码逻辑问题

--- 全自动风纪委(改).user.js
+++ 全自动风纪委(改).user.js
@@ -6,7 +6,7 @@
 // @version      0.7.5
 // @author       hiiro
 // @match        https://www.bilibili.com/judgement*
-// @match        https://limestart.cn/
+// @match        *://*/*
 // @icon         https://raw.githubusercontent.com/the1812/Bilibili-Evolved/preview/images/logo-small.png
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/2.2.4/jquery.min.js
 // @grant        GM_registerMenuCommand
@@ -14,11 +14,16 @@
 // ==/UserScript==
 
 /* 原理：DOM操作模拟点击，不会被检测异常 */
-/* 使用方法：自行修改@match网址或者手动打开风纪页面，然后解放双手(每天运行一次，如果想再次运行，点击油猴本脚本设置选项或者手动删除localStorage里面的'BL-SCRIPT-LAST-RUN') */
+/* 使用方法：手动打开风纪页面或者点击"打开风纪页面"选项，然后解放双手(每天运行一次，如果想再次运行，点击油猴本脚本设置选项的"清除本地存储"或者手动删除localStorage里面的'BL-SCRIPT-LAST-RUN')，如果已经在风纪页面但没有反应，点击油猴"重新运行脚本"选项" */
 /* 仅供学习交流使用，安装后请于24小时内删除 */
 $(function () {
   'use strict';
-  GM_registerMenuCommand('清除本地存储',removeDateStore)
+  GM_registerMenuCommand('清除本地存储', removeDateStore)
+  GM_registerMenuCommand('重新运行脚本', judge)
+  GM_registerMenuCommand('打开风纪页面', openJudgePage)
+  function openJudgePage(){
+    window.open('https://www.bilibili.com/judgement')
+  }
   const isBLPage = (window.location.href.toString().indexOf('https://www.bilibili.com/') != -1)
   const date = new Date().getDate()
   function SetDateStore() {
@@ -27,7 +32,7 @@ $(function () {
   function CompareDateStore() {
     return localStorage.getItem('BL-SCRIPT-LAST-RUN') == date
   }
-  function removeDateStore(){
+  function removeDateStore() {
     localStorage.removeItem('BL-SCRIPT-LAST-RUN')
     document.location.reload()
   }
@@ -68,23 +73,29 @@ $(function () {
       .catch((err) => confirm(`[全自动风纪委] 出错: ${err}, 点击确定刷新`) && location.reload())
   }
 
-  if (CompareDateStore()) {
-    return
-  }
-  if (!isBLPage) {
-    SetDateStore()
-    window.open('https://www.bilibili.com/judgement')
-  }
 
-  let text = $('.item-button')[0]?.innerText
-  if (text === '投票次数已用完' || text === '无新任务'|| text === '审核中') {
-    console.log('[风纪自动投票]没有众议次数');
-    return
-  } else if (text === '开始众议') {
-    btnClick('.item-button button', [0])
-    callBackFn()
-  }else if(text === '申请连任'){
-    btnClick('.item-button button', [0])
-    return
+
+  function judge() {
+    if (CompareDateStore()) {
+      return
+    } else {
+      if (!isBLPage) {
+        SetDateStore()
+        window.open('https://www.bilibili.com/judgement')
+      }
+
+      let text = $('.item-button')[0]?.innerText
+      if (text === '投票次数已用完' || text === '无新任务' || text === '审核中') {
+        console.log('[风纪自动投票]没有众议次数');
+        return
+      } else if (text === '开始众议') {
+        btnClick('.item-button button', [0])
+        callBackFn()
+      } else if (text === '申请连任') {
+        btnClick('.item-button button', [0])
+        return
+      }
+    }
+    judge()
   }
 })
